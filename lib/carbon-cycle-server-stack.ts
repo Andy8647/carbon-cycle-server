0f6b5e044bebad00193ee14ebcf0f54f8419f03a
Andy8647 <andy8647lgc@gmail.com>
2024-09-04 17:14:21 -0400

    update cdk

--- lib/carbon-cycle-server-stack.ts
+++ lib/carbon-cycle-server-stack.ts
@@ -1,16 +1,97 @@
 import * as cdk from 'aws-cdk-lib';
+import * as elasticbeanstalk from 'aws-cdk-lib/aws-elasticbeanstalk';
+import * as rds from 'aws-cdk-lib/aws-rds';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as s3assets from 'aws-cdk-lib/aws-s3-assets';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as path from 'path';
 
 export class CarbonCycleServerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create VPC
+    const vpc = new ec2.Vpc(this, 'CarbonCycleVPC', {
+      maxAzs: 2
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CarbonCycleServerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create RDS instance
+    const database = new rds.DatabaseInstance(this, 'CarbonCycleDatabase', {
+      engine: rds.DatabaseInstanceEngine.postgres({ version: rds.PostgresEngineVersion.VER_13 }),
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
+      vpc,
+      vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
+      databaseName: 'carboncycle',
+      credentials: rds.Credentials.fromGeneratedSecret('carboncycleuser'),
+    });
+
+    // Package NestJS application
+    const appAsset = new s3assets.Asset(this, 'NestJSAppZip', {
+      path: path.join(__dirname, '..', 'api'),
+    });
+
+    // Create Elastic Beanstalk app
+    const app = new elasticbeanstalk.CfnApplication(this, 'CarbonCycleApp', {
+      applicationName: 'carbon-cycle-app',
+    });
+
+    // Create application version
+    const appVersionProps = new elasticbeanstalk.CfnApplicationVersion(this, 'AppVersion', {
+      applicationName: app.applicationName || 'carbon-cycle-app',
+      sourceBundle: {
+        s3Bucket: appAsset.s3BucketName,
+        s3Key: appAsset.s3ObjectKey,
+      },
+    });
+
+    // Create Elastic Beanstalk environment
+    const environment = new elasticbeanstalk.CfnEnvironment(this, 'CarbonCycleEnvironment', {
+      environmentName: 'CarbonCycleEnvironment',
+      applicationName: app.applicationName || 'carbon-cycle-app',
+      solutionStackName: '64bit Amazon Linux 2 v5.8.0 running Node.js 18',
+      optionSettings: [
+        {
+          namespace: 'aws:autoscaling:launchconfiguration',
+          optionName: 'IamInstanceProfile',
+          value: 'aws-elasticbeanstalk-ec2-role',
+        },
+        {
+          namespace: 'aws:ec2:instances',
+          optionName: 'InstanceTypes',
+          value: 't3.micro',
+        },
+        {
+          namespace: 'aws:elasticbeanstalk:application:environment',
+          optionName: 'DATABASE_HOST',
+          value: database.dbInstanceEndpointAddress,
+        },
+        {
+          namespace: 'aws:elasticbeanstalk:application:environment',
+          optionName: 'DATABASE_PORT',
+          value: database.dbInstanceEndpointPort,
+        },
+        {
+          namespace: 'aws:elasticbeanstalk:application:environment',
+          optionName: 'DATABASE_NAME',
+          value: 'carboncycle',
+        },
+      ],
+      versionLabel: appVersionProps.ref,
+    });
+
+    // Ensure the environment is created after the application version
+    environment.addDependency(appVersionProps);
+
+    // Output the database endpoint
+    new cdk.CfnOutput(this, 'DatabaseEndpoint', {
+      value: database.dbInstanceEndpointAddress,
+      description: 'The endpoint of the database',
+    });
+
+    // Output the Elastic Beanstalk environment URL
+    new cdk.CfnOutput(this, 'EnvironmentUrl', {
+      value: `http://${environment.attrEndpointUrl}`,
+      description: 'The URL of the Elastic Beanstalk environment',
+    });
   }
 }
